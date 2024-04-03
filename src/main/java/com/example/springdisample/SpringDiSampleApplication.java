1b71f529d7dbf163e2bfb33dfb12822124627584
wild-mental <wild.mental@kidsnote.com>
2024-04-04 06:11:33 +0900

    initial commit 2

---
+++ src/main/java/com/example/springdisample/SpringDiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiSampleApplication.class, args);
+    }
+
+}
