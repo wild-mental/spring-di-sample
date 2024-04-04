b0d4881d85ceb5b8eb60dec957a1b3b592f047dc
wild-mental <wild.mental@kidsnote.com>
2024-04-05 06:59:51 +0900

    DI checking 업데이트 - Broadcast 체크

--- src/main/java/com/example/springdisample/main/DiCheckMain.java
+++ src/main/java/com/example/springdisample/main/DiCheckMain.java
@@ -1,12 +1,19 @@
 package com.example.springdisample.main;
 
+import com.example.springdisample.obj.Broadcast;
 import com.example.springdisample.obj.Singer;
+import com.example.springdisample.obj.Stage;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class DiCheckMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        Singer singer = (Singer)context.getBean("singer");
-        singer.sing();
+//        Singer singer = (Singer)context.getBean("singer");
+//        singer.sing();
+        // Broadcast 만 테스트 하면 전체 주입 확인 가능
+        Broadcast broadcast = (Broadcast)context.getBean("broadcast");
+        broadcast.startBroadcasting();
+        broadcast.continueBroadcasting();
+        context.close();
     }
 }
