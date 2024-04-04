dd16645c6f013c5a1fd2e0c9601ff1cc3387756c
wild-mental <wild.mental@kidsnote.com>
2024-04-05 06:50:47 +0900

    DI checking 업데이트 - rename

--- src/main/java/com/example/springdisample/main/DiCheckMain.java
+++ src/main/java/com/example/springdisample/main/DiCheckMain.java
@@ -3,7 +3,7 @@ package com.example.springdisample.main;
 import com.example.springdisample.obj.Singer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class DiMain {
+public class DiCheckMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         Singer singer = (Singer)context.getBean("singer");
